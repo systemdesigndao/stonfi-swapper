882ed7b21ab9f42279893aad9d43aebf533b1a34
Maxim Chernyshov <dsgnrvd@gmail.com>
2023-08-05 21:18:53 +0400

    add index.js, Home.js chunks.
    dist/assets/index.js   1,256.48 kB │ gzip: 389.78 kB
    ->
    dist/assets/index.js     186.35 kB │ gzip:  60.81 kB
    dist/assets/Home.js    1,092.77 kB │ gzip: 337.38 kB

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,30 @@
 import { createApp } from "vue";
 import "./style.css";
+// import Home from "./Home.vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import Notifications from "@kyvg/vue3-notification";
 import { Buffer } from "buffer";
 import "flowbite";
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const Home = () => import("./Home.vue");
 
 window.Buffer = Buffer;
 
 const pinia = createPinia();
+
+const routes = [{ path: "/", component: Home }];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
 const app = createApp(App);
 
 app.use(pinia);
 app.use(Notifications, { name: "alert" });
+app.use(router);
+
 app.mount("#app");
